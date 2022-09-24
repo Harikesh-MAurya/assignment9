c5e6827062263fde722ee051ffae35b2bb4b1bc6
Harikesh-MAurya <113936189+Harikesh-MAurya@users.noreply.github.com>
2022-09-24 15:35:16 +0530

    Add files via upload

---
+++ assignment9.py
@@ -0,0 +1,87 @@
+# 1. Write a python script to print MySirG N times on the screen
+# 2. Write a python script to print first N natural numbers
+# 3. Write a python script to print first N natural numbers in reverse order
+# 4. Write a python script to print first N odd natural numbers
+# 5. Write a python script to print first N odd natural numbers in reverse order
+# 6. Write a python script to print first N even natural numbers
+# 7. Write a python script to print first N even natural numbers in reverse order
+# 8. Write a python script to print squares of first N natural numbers
+# 9. Write a python script to print cubes of first N natural numbers
+# 10. Write a python script to print first 10 multiples of N
+
+
+# 1) ..........................................................
+# n=int(input("Enter n : "))
+# i=1
+# while i<=n:
+#     print("MySirG")
+#     i=i+1
+
+
+# 2) .........................................................
+# n=int(input("Enter n : "))
+# i=1
+# while i<=n:
+#     print(i)
+#     i=i+1
+
+
+# 3) ........................................................
+# n=int(input("Enter n : "))
+# while n>=1:
+#     print(n,end=" ")
+#     n=n-1
+
+
+# 4) ..........................................................
+# n=int(input("Enter n : "))
+# i=1
+# while i<=n:
+#     print(i,end=" ")
+#     i=i+2
+
+
+# 5) .........................................................
+# n=int(input("Enter n : "))
+# while n>=1:
+#     print(n,end=" ")
+    # n=n-2
+
+
+# 6) .........................................................
+# n=int(input("Enter n : "))
+# i=1
+# while i<=n:
+#     print(2*i,end=" ")
+#     i=i+1
+
+
+# 7) ..........................................................
+# n=int(input("Enter n : "))
+# while n>=1:
+#     print(2*i,end=" ")
+#     n=n-1
+
+
+# 8) ........................................................
+# n=int(input("Enter n : "))
+# i=1
+# while i<=n:
+#     print(pow(i,2),end=" ")
+#     i=i+1
+
+
+# 9) ................................................................
+# n=int(input("Enter n : "))
+# i=1
+# while i<=n:
+#     print(pow(i,3),end=" ")
+#     i=i+1
+
+
+# 10) ..............................................................
+n=int(input("Enter n : "))
+i=1
+while i<=n:
+    print(10*i,end=" ")
+    i=i+1
